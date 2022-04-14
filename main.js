1b498b267bd41c9d0d79de5fb8d5275a856e2f3b
loczzs <le.baoloczzs@gmail.com>
2022-04-14 14:20:52 +0700

    style --

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
 // 1 chương trình tính tiền lương
+
 // var salary = parseFloat(100.000)
 // var day = Number(prompt("Nhập số ngày làm : "))
 // var sum = (salary * day)
 
 // console.log("số tiền lương là : "+ (sum)+ ".000")
-
+// ------------------------------------------------------------------
 
 // 2 tính giá trị trung bình
+
 // var a = Number(prompt("Nhập vào số thực : "))
 // var b = Number(prompt("Nhập vào số thực : "))
 // var c = Number(prompt("Nhập vào số thực : "))
@@ -14,20 +16,24 @@
 // var e = Number(prompt("Nhập vào số thực : "))
 // var average = Number((a+b+c+d+e)/5)
 // console.log(average)
-
+// --------------------------------------------------------------------
 // 3 Quy đổi tiền
+
 // var USD = Number(prompt("Nhập só USD : "))
 // var VND = 23500
 // var translate = Number(USD * VND)
 // console.log("Người dùng nhập " + USD + " USD => Xuất ra " + VND +" VND " )
-
+// ------------------------------------------------------------------------
 // 4 tính diện tích , chu vi hình chữ nhật
+
 // var width = Number(prompt("Nhập chiều rộng: "))
 // var leght = Number(prompt("Nhập chiều dài: "))
 // var acreage =(width * leght)
 // var perimeter = (( width + leght )*2)
 // console.log("Diện tích HCN : " + acreage )
 // console.log(" Chu vi HCN : "  + perimeter )
+// ---------------------------------------------------------------------------
+
 // 5 tính tổng 2 ký số
 
 // var n = Number(prompt("Nhập vào 1 số có 2 chữ số (VD: 12 ,44 ,83) "))
